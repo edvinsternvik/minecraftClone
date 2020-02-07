b7a860681ffd12e1b556099becd9c338fcb49b86
RedE01 <e.sternvik@gmail.com>
2020-02-07 16:40:38 +0100

    Add debug function timer

---
+++ src/Debug.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <chrono>
+#include <iostream>
+
+class FunctionTimer {
+public:
+	FunctionTimer() : start(std::chrono::high_resolution_clock::now()) {
+
+	}
+
+	~FunctionTimer() {
+		std::chrono::high_resolution_clock::time_point stop = std::chrono::high_resolution_clock::now();
+		auto time = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+		std::cout << "Function time: " << time.count() << std::endl;
+	}
+
+private:
+	std::chrono::high_resolution_clock::time_point start;
+};
